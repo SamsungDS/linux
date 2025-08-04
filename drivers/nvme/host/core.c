689fb911eeda5d420b9717da94b91a7933e90f82
Christoph Hellwig <hch@lst.de>
2025-08-04 07:07:32 -0700

    TEMP: nvme: disable discard
    The qemu T10PI support generates incorrect guard tags on discarded
    logical blocks.  Hack around this by disabling discard.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -2383,7 +2383,7 @@ static int nvme_update_ns_info_block(struct nvme_ns *ns,
 	if (!nvme_update_disk_info(ns, id, &lim))
 		capacity = 0;
 
-	nvme_config_discard(ns, &lim);
+//	nvme_config_discard(ns, &lim);
 	if (IS_ENABLED(CONFIG_BLK_DEV_ZONED) &&
 	    ns->head->ids.csi == NVME_CSI_ZNS)
 		nvme_update_zone_info(ns, &lim, &zi);
