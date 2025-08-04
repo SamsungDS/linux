b57f6fdd2faeabe6ebb346bab8b1c7927a9fd0e8
Goldwyn Rodrigues <rgoldwyn@suse.com>
2025-08-04 07:13:15 -0700

    iomap: add a bioset pointer to iomap_read_folio_ops
    Optionally allocate the bio from the bioset provided in
    iomap_read_folio_ops.  If no bioset is provided, fs_bio_set is still
    used, which is the standard bioset for filesystems.
    
    Signed-off-by: Goldwyn Rodrigues <rgoldwyn@suse.com>
    [hch: factor out two helpers]
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- include/linux/iomap.h
+++ include/linux/iomap.h
@@ -347,6 +347,12 @@ struct iomap_read_folio_ops {
 	 */
 	void (*submit_io)(const struct iomap_iter *iter, struct bio *bio,
 			  loff_t file_offset);
+
+	/*
+	 * Optional, allows filesystem to specify own bio_set, so new bio's
+	 * can be allocated from the provided bio_set.
+	 */
+	struct bio_set *bio_set;
 };
 
 int iomap_read_folio(struct folio *folio, const struct iomap_ops *ops,
