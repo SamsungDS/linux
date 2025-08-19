fbe9c1877106d2cc67e8f9640dd75cc89a8863a2
Christoph Hellwig <hch@lst.de>
2025-08-19 07:19:44 +0200

    block: make bio auto-integrity deadlock safe
    The current block layer automatic integrity protection allocates the
    actual integrity buffer, which has three problems:
    
     - because it happens at the bottom of the I/O stack and doesn't use a
       mempool it can deadlock under load
     - because the data size in a bio is almost unbounded when using lage
       folios it can relatively easily exceed the maximum kmalloc size
     - even when it does not exceed the maximum kmalloc size, it could
       exceed the maximum segment size of the device
    
    Fix this by limiting the I/O size so that we can allocated at least a
    2MiB integrity buffer, i.e. 128MiB for 8 byte PI and 512 byte integrity
    internals, and create a mempool as a last resort for this maximum size,
    mirroring the scheme used for bvecs.  As a nice upside none of this
    can fail now, so we remove the error handling and open code the
    trivial addition of the bip vec.
    
    Fixes: 7ba1ba12eeef ("block: Block layer data integrity support")
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- block/blk-settings.c
+++ block/blk-settings.c
@@ -183,6 +183,17 @@ static int blk_validate_integrity_limits(struct queue_limits *lim)
 	if (!bi->interval_exp)
 		bi->interval_exp = ilog2(lim->logical_block_size);
 
+	/*
+	 * The block layer automatically adds integrity data for bios that don't
+	 * already have it.  It allocates a single segment. Limit the I/O size
+	 * so that a single maximum size metadata segment can cover the
+	 * integrity data for the entire I/O.
+	 */
+	lim->max_sectors = min3(lim->max_sectors,
+		BLK_INTEGRITY_MAX_SIZE /
+			bi->pi_tuple_size * lim->logical_block_size,
+		lim->max_segment_size >> SECTOR_SHIFT);
+
 	return 0;
 }
 
