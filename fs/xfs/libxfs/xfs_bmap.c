addf48d5d380a3db8207f6eb66494ea886bd1ca8
Kanchan Joshi <joshi.k@samsung.com>
2026-04-17 16:35:09 +0530

    xfs: write stream based AG placement
    When write stream is set on the file, override the default
    directory-locality heuristic with a new heuristic that maps
    available AGs into streams.
    
    Isolating distinct write streams into dedicated allocation groups helps
    in reducing the block interleaving of concurrent writers. Keeping these
    streams spatially separated reduces AGF lock contention and logical file
    fragmentation.
    
    If AGs are fewer than write streams, write streams are distributed into
    available AGs in round robin fashion.
    If not, available AGs are partitioned into write streams. Since each
    write stream maps to a partition of multiple contiguous AGs, the inode hash
    is used to choose the specific AG within the stream partition. This can
    help with intra-stream concurency when multiple files are being written in
    a single stream that has 2 or more AGs.
    
    Example: 8 Allocation Groups, 4 Streams
    Partition Size = 2 AGs per Stream
    
       Stream 1 (ID: 1)         Stream 2 (ID: 2)         Streams 3 & 4
     +---------+---------+    +---------+---------+    +-------------
     |   AG0   |   AG1   |    |   AG2   |   AG3   |    |  AG4...AG7
     +---------+---------+    +---------+---------+    +-------------
          ^         ^              ^         ^
          |         |              |         |
          | File B (ino: 101)      | File D (ino: 201)
          | 101 % 2 = 1 -> AG 1    | 201 % 2 = 1 -> AG 3
          |                        |
     File A (ino: 100)        File C (ino: 200)
     100 % 2 = 0 -> AG 0      200 % 2 = 0 -> AG 2
    
    If AGs can not be evenly distributed among streams, the last stream will
    absorb the remaining AGs.
    
    Note that there are no hard boundaries; this only provides explicit
    routing hint to xfs allocator so that it can group/isolate files in the way
    application has decided to group/isolate. We still try to preserve file
    contiguity, and the full space can be utilized even with a single stream.
    
    Signed-off-by: Kanchan Joshi <joshi.k@samsung.com>

--- fs/xfs/libxfs/xfs_bmap.c
+++ fs/xfs/libxfs/xfs_bmap.c
@@ -3206,6 +3206,43 @@ xfs_default_ag_set_size(
 }
 
 static xfs_agnumber_t
+xfs_inode_write_stream_ag_set(
+	struct xfs_inode	*ip,
+	xfs_agnumber_t		base_agno)
+{
+	struct xfs_mount	*mp = ip->i_mount;
+	uint32_t		nr_streams = xfs_inode_max_write_streams(ip);
+	uint32_t		stream_id = ip->i_write_stream;
+	uint32_t		nr_ags = mp->m_sb.sb_agcount;
+	xfs_agnumber_t		set_size, target_agno = base_agno;
+
+
+	if (!nr_streams) {
+		set_size = xfs_default_ag_set_size(ip);
+		goto out;
+	}
+
+	stream_id -= 1; /* For 0-based math; stream-ids are 1-based */
+	set_size = nr_ags / nr_streams;
+
+	if (set_size) {
+		target_agno = stream_id * set_size;
+		/* unven distribution, last stream will cover extra AGs */
+		if (stream_id == nr_streams - 1)
+			set_size = nr_ags - target_agno;
+	} else {
+		/* for the case when we have fewer AGs than streams */
+		target_agno = stream_id % nr_ags;
+		set_size = 1;
+	}
+
+out:
+	return (target_agno + (XFS_INO_TO_AGINO(mp, ip->i_ino) % set_size)) %
+		mp->m_sb.sb_agcount;
+
+}
+
+static xfs_agnumber_t
 xfs_ag_to_ag_set(
 	struct xfs_bmalloca	*ap,
 	xfs_agnumber_t		base_agno)
@@ -3217,6 +3254,9 @@ xfs_ag_to_ag_set(
 	/* Apply fanning only for regular file data */
 	if (!(ap->datatype & XFS_ALLOC_USERDATA))
 		return base_agno;
+
+	if (ip->i_write_stream)
+		return xfs_inode_write_stream_ag_set(ip, base_agno);
 
 	set_size = xfs_default_ag_set_size(ip);
 	/*
